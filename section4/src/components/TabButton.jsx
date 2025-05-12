0410fd767ffa3181ab9dde9ad5f975fce60777f3
shunsaku-sugita <saku.dot@gmail.com>
2025-05-12 16:13:29 -0700

    Chapter 66

--- section4/src/components/TabButton.jsx
+++ section4/src/components/TabButton.jsx
@@ -1,10 +1,12 @@
-export default function TabButton({ children, onSelect, isSelected }) {
-  console.log('TABBUTTON COMPONENT EXECUTING');
+const TabButton = ({ children, isSelected, ...props }) => {
+  console.log("TABBUTTON COMPONENT EXECUTING");
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+      <button className={isSelected ? "active" : undefined} {...props}>
         {children}
       </button>
     </li>
   );
-}
+};
+
+export default TabButton;
