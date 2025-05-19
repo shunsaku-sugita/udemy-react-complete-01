14bcac8a2a9311be5ff0ce432c1628d66bc94d4c
shunsaku-sugita <saku.dot@gmail.com>
2025-05-18 21:52:23 -0700

    Chapter 80

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import Player from "./components/Player";
-
+import GameBoard from "./components/GameBoard";
 function App() {
   return (
     <main>
@@ -8,6 +8,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
